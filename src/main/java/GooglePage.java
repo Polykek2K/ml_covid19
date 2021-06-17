2e5029b301f995964599ef7f424088bcff979623
Katerina <zaitceva_ek@mail.ru>
2021-06-18 02:27:04 +0300

    #116 Created Selenide project structure try2.

--- src/main/java/GooglePage.java
+++ src/main/java/GooglePage.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.By;
 import static com.codeborne.selenide.Selenide.$;
 
 public class GooglePage {
-    //
+
     public void searchFor(String text) {
         $(By.name("q")).val(text).pressEnter();
     }
